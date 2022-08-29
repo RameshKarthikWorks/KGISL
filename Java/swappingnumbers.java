a57390c3081dda527e18d3902fa3cee869a4f24a
RameshKarthiWorks <ramesharavindhkarthikeyan@gmail.com>
2022-08-29 08:43:30 +0530

    Assessment Folder commit on 29/08/2022

---
+++ Java/swappingnumbers.java
@@ -0,0 +1,18 @@
+public class changingnumbers
+{
+
+public static void main (String args[])
+{
+int a=10;
+int b=20;
+
+
+b=b+a; // 20=20+10=30
+a=b-a; // 10=30-10=20
+b=b-a; // 30=30-20=10
+
+System.out.println("The swapped numbers are a is" a+" "+b);
+}
+}
+
+
