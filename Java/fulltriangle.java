81d54e2b5036e0f5dbb55172fc84e7f0cbcd9813
RameshKarthiWorks <ramesharavindhkarthikeyan@gmail.com>
2022-09-01 08:48:31 +0530

    Commiited changes on 01/09/2022

---
+++ Java/fulltriangle.java
@@ -0,0 +1,18 @@
+public class fulltriangle
+{  
+   public static void main (String args[])
+   {
+	   int i,j,n=5;
+	   for( i=0;i<=n; i++)
+	   {
+		   System.out.print("s ");
+	   }
+	   for ( j=0;j<=i; j++)
+	   {
+		   System.out.println("* ");
+	   }
+	   System.out.println();
+   }
+}
+
+		   
---
+++ Java/storingitems.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class storingitems
+{
+
+public static void main (String args[])
+{
+	
+	System.out.println("Enter the array value:");
+	Scanner arr[] = new Scanner(System.in);
+	
+	int n[] = arr[].nextInt();
+	
+System.out.println(n[]);
+
+
+}
+}
+
